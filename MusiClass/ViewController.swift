80d8de0e577ed6d93a9ef1490463157e6bf5ea22
Rodrigo Bueno Tomiosso <mourodrigo_tres@new-host-5.home>
2017-03-25 13:39:27 -0300

    Initial Commit

---
+++ MusiClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusiClass
+//
+//  Created by Rodrigo Bueno Tomiosso on 25/03/17.
+//  Copyright © 2017 mourodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
